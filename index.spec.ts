9f4fb4836d28e909c29c6e89b95ccdcfd65ddaa6
runyuw <wang.runy@northeastern.edu>
2021-10-05 15:28:55 -0400

    First Commit

---
+++ index.spec.ts
@@ -0,0 +1,15 @@
+
+import { MyProject } from './index';
+import { expect } from 'chai';
+
+describe('MyProject', (): void => {
+
+  describe('myMethod()', () => {
+
+    it('should return "Hello, World!"', (): void => {
+      expect(MyProject.myMethod('', 0)).to.equal('Hello, World!');
+    });
+
+  });
+
+});
---
+++ index.ts
@@ -0,0 +1,17 @@
+
+/**
+ * Sample TypeDoc comment for class MyProject
+ */
+export class MyProject {
+
+  /**
+   * sample TypeDoc comment for my method.
+   * Params and Returns will be auto-generated
+   */
+  public static myMethod = (arg1: string, arg2: number): string => {
+    return 'Hello, World!';
+  }
+
+}
+
+console.log(MyProject.myMethod('', 0));
